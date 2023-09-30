465a0caf40e36cc11581876edf4fb9ca938d0ee7
Y_Nishino <kaek33586@gmail.com>
2023-09-30 12:53:48 +0900

    add plan list

--- app/src/main/java/com/mrfrogman/traveller2/MainActivity.kt
+++ app/src/main/java/com/mrfrogman/traveller2/MainActivity.kt
@@ -4,8 +4,10 @@ import android.os.Bundle
 import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
@@ -13,9 +15,9 @@ import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Add
-import androidx.compose.material.icons.filled.Person
 import androidx.compose.material.icons.filled.Settings
-import androidx.compose.material3.Button
+import androidx.compose.material3.Card
+import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.CenterAlignedTopAppBar
@@ -31,6 +33,7 @@ import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.sp
 import com.mrfrogman.traveller2.ui.theme.TravellerTheme
 
 class MainActivity : ComponentActivity() {
@@ -64,45 +67,12 @@ fun MainScreen() {
                     IconButton(onClick = { /* doSomething() */ }) { Icon(
                             imageVector = Icons.Filled.Settings,
                             contentDescription = "Settings Button"
-                )}})},
-        content = {innerPadding ->
-            LazyColumn(
-                contentPadding = innerPadding,
-                verticalArrangement = Arrangement.spacedBy(8.dp)
-            ) {
-                val list = (0..20).map { it.toString() }
-                items(count = list.size) {
-                    Column {
-                        Text(
-                            text = list[it],
-                            style = MaterialTheme.typography.bodyLarge,
-                            modifier = Modifier
-                                .fillMaxWidth()
-                                .padding(horizontal = 16.dp))
-                        Row {
-                            Text(
-                                text = "list[it]",
-                                style = MaterialTheme.typography.bodyLarge,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(horizontal = 16.dp))
+                )}})
+        },
+        content = {
+                Card(it)
+        },
 
-                            Text(
-                                text = "list[it]2",
-                                style = MaterialTheme.typography.bodyLarge,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(horizontal = 16.dp))
-
-                            Text(
-                                text = "list[it]3",
-                                style = MaterialTheme.typography.bodyLarge,
-                                modifier = Modifier
-                                    .fillMaxWidth()
-                                    .padding(horizontal = 16.dp))
-                        }
-                    }
-                } } },
         floatingActionButton = {
             ExtendedFloatingActionButton(
                 icon = { Icon(Icons.Filled.Add,"Add Button") },
@@ -111,6 +81,62 @@ fun MainScreen() {
                     Log.d("test", "MainScreen: clicked add plan button")
         })}
     )
+}
+
+@Composable
+private fun Card(paddingValues: PaddingValues) {
+    LazyColumn(
+        Modifier.padding(paddingValues),
+        verticalArrangement = Arrangement.spacedBy(8.dp)
+    ) {
+        val list = (0..20).map { it.toString() }
+        items(count = list.size) {
+            Card(
+                colors = CardDefaults.cardColors(
+                    containerColor = MaterialTheme.colorScheme.primary
+                ),
+                modifier = Modifier.padding(vertical = 4.dp, horizontal = 8.dp)
+            ) {
+                CardContent(it)
+            }
+        }
+    }
+}
+
+@Composable
+private fun CardContent(i: Int) {
+    Column(
+        Modifier.background(color = MaterialTheme.colorScheme.primary).padding(vertical = 8.dp)
+    ) {
+        Text(
+            text = "Title",
+            fontSize = 20.sp,
+            style = MaterialTheme.typography.bodyLarge,
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(horizontal = 16.dp))
+        Row {
+            Text(
+                text = i.toString(),
+                fontSize = 18.sp,
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp))
+
+            Text(
+                text = "123456",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp)
+                    .weight(1f))
+
+            Text(
+                text = "2023/09/30",
+                style = MaterialTheme.typography.bodyLarge,
+                modifier = Modifier
+                    .padding(horizontal = 16.dp))
+        }
+    }
 }
 
 @Preview(showBackground = true, widthDp = 340)
