9ce04715cdb06352a562e5eb0ff193a0c1b2bf2a
Y_Nishino <kaek33586@gmail.com>
2023-12-16 17:45:24 +0900

    add new view

--- app/src/main/java/com/mrfrogman/traveller2/views/AddPayView.kt
+++ app/src/main/java/com/mrfrogman/traveller2/views/AddPayView.kt
@@ -2,25 +2,106 @@ package com.mrfrogman.traveller2.views
 
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.rememberScrollState
+import androidx.compose.foundation.text.KeyboardActions
+import androidx.compose.foundation.verticalScroll
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBackIosNew
+import androidx.compose.material3.Button
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalFocusManager
+import androidx.compose.ui.platform.LocalSoftwareKeyboardController
+import androidx.compose.ui.text.input.KeyboardType
+import androidx.core.text.isDigitsOnly
 import androidx.navigation.NavHostController
+import com.mrfrogman.traveller2.views.compose.TicketTextField
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun AddPayView(
     navController: NavHostController,
 ) {
+    val scrollState = rememberScrollState()
+    var amount by remember { mutableStateOf("") }
+    val focusManager = LocalFocusManager.current
     Scaffold(
         topBar = {
-            Text(text = "支払いの追加")
+            CenterAlignedTopAppBar(
+                title = { Text(text = "支払いの追加") },
+                navigationIcon = {
+                    IconButton(
+                        onClick = {
+                            navController.navigateUp()
+                        }
+                    ) {
+                        Icon(
+                            imageVector = Icons.Filled.ArrowBackIosNew,
+                            contentDescription = "Back screen button"
+                        )
+                    }
+                },
+                actions = {
+                    Button(
+                        onClick = {
+                            navController.navigateUp()
+                        }
+                    ){
+                        Text(text = "作成")
+                    }
+                }
+            )
         }
     ) { innerPadding ->
         Column(
-            modifier = Modifier.padding(innerPadding)
+            modifier = Modifier
+                .padding(innerPadding)
+                .verticalScroll(scrollState),
+            horizontalAlignment = Alignment.CenterHorizontally
         ) {
-            
+            TicketTextField(
+                contentDescription = "Input ",
+                label = "合計金額",
+                value = amount,
+                keyboardType = KeyboardType.Number,
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        focusManager.clearFocus()
+                    }
+                ),
+                onValueChange = {
+                    if (it.isDigitsOnly()) {
+                        amount = it
+                    }
+                },
+            )
+            TicketTextField(
+                contentDescription = "Input ",
+                label = "合計金額",
+                value = amount,
+                keyboardType = KeyboardType.Number,
+                keyboardActions = KeyboardActions(
+                    onDone = {
+                        focusManager.clearFocus()
+                    }
+                ),
+                onValueChange = {
+                    if (it.isDigitsOnly()) {
+                        amount = it
+                    }
+                },
+            )
         }
     }
 }
